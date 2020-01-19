105c2e3069c55b9eef6be21526f45c7abd14105b
Jarrod <jarrod.pan@gmail.com>
2020-01-19 18:59:04 +1100

    added card to layout

--- template/index.php
+++ template/index.php
@@ -1,6 +1,36 @@
 <?php
 global $conn;
 
-var_dump(Item::getLastest($conn));
+//var_dump(Item::getLastest($conn));
 
 ?>
+
+<style>
+.card-price {
+	padding-top:inherit;
+	padding-bottom:inherit;
+	text-align: right;
+}
+
+</style>
+
+<div class='container-fluid'>
+	<div class='row'>
+		<div class='col-sm-3'>
+			<div class=' card'>
+			<img class="card-img-top" src="/res/items/1.jpg" alt="Card image">
+			
+			<div class='card-body d-flex'>
+				<div class='flex-grow-1'>
+				<h4 class='card-title'>Album</h4>
+				<p class='card-text'>Product</p>
+				</div>
+				<div class='card-price align-self-stretch'>
+				$100
+				</div>
+			</div>
+			</div>
+		</div>
+		
+	</div>
+</div>
